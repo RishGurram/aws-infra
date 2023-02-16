7f9c72954641d2112950b485e6c3634cfe202ca5
Rishika Gurram <gurram.ri@northeastern.edu>
2023-02-15 19:28:46 -0500

    test 2

--- variable.tf
+++ variable.tf
@@ -26,13 +26,13 @@ variable "create_cidr" {
 }
 
 variable "public_subnet_cidr" {
-  type = list(any)
-  default = ["10.0.1.0/24","10.0.2.0/24", "10.0.3.0/24"]
+  type    = list(any)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidr" {
-  type = list(any)
-  default = ["10.0.4.0/24","10.0.5.0/24", "10.0.6.0/24"]
+  type    = list(any)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 }
 
 locals {
